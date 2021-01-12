5bbf37d1c3ca80d74c42bf1656feccee6494a1d8
yukesu26 <yusuke.562139@gmail.com>
2021-01-12 18:33:22 +0900

    新規登録機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nick_name, presence: true
+  validates :email, presence: true, uniqueness: true 
+  validates :encrypted_password, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
+
+  has_many :products
+  has_many :buys
+
 end
