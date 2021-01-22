2237b4bb921c14dfd2e07df36717444fe4f567a0
yukesu26 <yusuke.562139@gmail.com>
2021-01-22 17:47:59 +0900

    一覧表示実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   def index
-    #@products = Product.all
+    @products = Product.all.order(created_at: :desc)
   end
 
   def new
