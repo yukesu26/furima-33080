51730664b27035c6e2062677d461b6106330f7d5
yukesu26 <yusuke.562139@gmail.com>
2021-01-23 13:53:42 +0900

    showページに画像を表示させる

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index" # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index, :new, :create]
+  resources :products, only: [:index, :new, :create, :show]
   
 end
