fb223cd8ac9247428bc420ae62ca27db468278cd
yukesu26 <yusuke.562139@gmail.com>
2021-01-27 02:18:48 +0900

    テストコード修正(クレカ決済導入前)

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -19,52 +19,52 @@ require 'rails_helper'
     end
   end
   context "商品が購入できない場合" do
-    it "郵便番号が空では登録できない" do
+    it "郵便番号が空では購入できない" do
       @order.post_code = ''
       @order.valid?
       expect(@order.errors.full_messages).to include("Post code can't be blank")
     end
-    it "郵便番号にハイフンがなければ登録できない" do
+    it "郵便番号にハイフンがなければ購入できない" do
       @order.post_code = '1113333'
       @order.valid?
       expect(@order.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
     end
-    it "都道府県が空では登録できない" do
+    it "都道府県が空では購入できない" do
       @order.shipping_place_id = 1
       @order.valid?
       expect(@order.errors.full_messages).to include("Shipping place can't be blank")
     end
-    it "市区町村が空では登録できない" do
+    it "市区町村が空では購入できない" do
       @order.city = ''
       @order.valid?
       expect(@order.errors.full_messages).to include("City can't be blank")
     end
-    it "番地が空では登録できない" do
+    it "番地が空では購入できない" do
       @order.address = ''
       @order.valid?
       expect(@order.errors.full_messages).to include("Address can't be blank")
     end
-    it "電話番号が空では登録できない" do
+    it "電話番号が空では購入できない" do
       @order.phone_number = ''
       @order.valid?
       expect(@order.errors.full_messages).to include("Phone number can't be blank")
     end
-    it "電話番号にハイフンがあると登録できない" do
+    it "電話番号にハイフンがあると購入できない" do
       @order.phone_number = '090-1234-1234'
       @order.valid?
       expect(@order.errors.full_messages).to include("Phone number is invalid")
     end
-    it "電話番号は11桁以内でなければ登録できない" do
+    it "電話番号は11桁以内でなければ購入できない" do
       @order.phone_number = '090123412345'
       @order.valid?
       expect(@order.errors.full_messages).to include("Phone number is invalid")
     end
-    it "郵便番号が英数混合だと登録できない" do
+    it "郵便番号が英数混合だと購入できない" do
       @order.post_code = '1a2-b3c4'
       @order.valid?
       expect(@order.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
     end
-    it "電話番号が英数混合では登録できない" do
+    it "電話番号が英数混合では購入できない" do
       @order.phone_number = '090s345k432'
       @order.valid?
       expect(@order.errors.full_messages).to include("Phone number is invalid")
