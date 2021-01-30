bcbde6db3f459a57892e257e2eedb7a3bf64f0fb
yukesu26 <yusuke.562139@gmail.com>
2021-01-30 14:01:37 +0900

    編集ページへの分岐制限を修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,7 @@ class ProductsController < ApplicationController
   end 
 
   def edit
-    if current_user.id != @product.user_id 
+    if current_user.id != @product.user_id || @product.buy.present?
       redirect_to root_path
     end
   end
